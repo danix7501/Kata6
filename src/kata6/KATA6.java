a62bc90fc39939fb42c732b5a71e0e02d0cf8764
usuario <usuario@10.22.143.32>
2015-12-03 09:00:36 +0000

    version1-kata6

---
+++ src/kata6/KATA6.java
@@ -0,0 +1,20 @@
+package kata6;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class KATA6 {
+
+    public static void main(String[] args) throws IOException {
+
+        String nameFile = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4-master\\DATA\\emailsfilev1.txt";
+        ArrayList<String> mailArray = MailListReader.read(nameFile);
+        System.out.println(mailArray.size());
+
+        Histogram<String> histogram = MailHistogramBuilder.build(mailArray);
+
+        new HistogramDisplay(histogram).execute();
+        
+    }
+
+}
